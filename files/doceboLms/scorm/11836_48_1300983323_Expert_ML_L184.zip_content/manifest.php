2eac4aea68170e1e6b9be577f9f14bff849fe09c
julien pouillard <j.pouillard@effigie-creations.com>
2011-05-02 14:21:33 +0200

    delete2

--- files/doceboLms/scorm/11836_48_1300983323_Expert_ML_L184.zip_content/manifest.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$Type = "Quizz";
-$Title = "Expert, Microlearning, Lesson 184";
-$Author = "WR: SM. PR: LF.";
-$Description = "We're glad to see that you're still hard at work. Here's our Word of the Day. You'll also hear a conversation about a problem in a restaurant. Later, we'll learn some business expressions.";
-$Version = "2.6.32.0";
-
-?>
